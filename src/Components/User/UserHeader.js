3eea21ae8d0d9dd8fc0cd9c8ead7fa78e9073713
João Padilha <jvlpadilha@outlook.com>
2022-07-05 16:39:16 -0300

    Mobile menu e user Area

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UserHeaderNav from './UserHeaderNav';
-
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router-dom';
 const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+  React.useEffect(() => {
+    if (location.pathname === '/conta') setTitle('Minha Conta');
+    if (location.pathname === '/conta/estatisticas') setTitle('Estatísticas');
+    if (location.pathname === '/conta/postar') setTitle('Poste Sua Foto');
+  }, [location]);
   return (
-    <header>
-      <h1 className="title">Título</h1>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
